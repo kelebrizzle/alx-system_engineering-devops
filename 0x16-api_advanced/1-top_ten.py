272e9877bf00bb235009335187489b60dc17c7d4
Kelechukwu <kelebrizzle@gmail.com>
2024-02-15 11:59:10 +0000

    fixed errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""This script queries the REDDIT API"""
+"""Query subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    This function prints the titles of the first
-    10 hot posts listed for a given subreddit.
-    """
-    base_url = 'https://www.reddit.com'
-    headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus\
-                5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko)\
-                Chrome/107.0.0.0 Mobile Safari/537.36'}
+    """Query Reddit and print titles of the first 10 hot posts"""
 
-    data = requests.get("{}/r/{}/hot.json"
-                        .format(base_url, subreddit), headers=headers,
-                        allow_redirects=False)
-    if data.status_code != 200:
-        print(None)
-    else:
-        result = data.json().get('data').get('children')
-        for i in range(0, 10):
-            print(result[i].get('data').get('title'))
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+
+    # Set a custom User-Agent to avoid too many requests error
+    headers = {'User-Agent': 'My user Agent 1.0'}
+
+    # send a GET request to the Reddit API
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
+        print("None")
